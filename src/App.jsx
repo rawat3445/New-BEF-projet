ff291e71e729e350ad9ad5607ef0034f83eb9d62
Kanhaiyasahu01 <kanhaiyasahutools@gmail.com>
2025-05-27 19:32:11 +0530

    Initial frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* It will have routes to every other industry  */}
+      <div>
+        Bharat Economic Forum
+      </div>
+    </>
+  )
+}
+
+export default App
