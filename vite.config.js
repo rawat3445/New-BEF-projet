affc5843fb8b0a65f9ec80c0567c4e348eb24f42
Suraj Rawat <surajrawat124421@gmail@.com>
2025-09-02 21:33:30 +0530

    fix base link

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],  // no base here
-  build: { outDir: "dist" }
+  base: '/bef-web-deploy/',
+  plugins: [react(), tailwindcss()],
 });
